d18c76f4a84a72d4cf26d5656e2202d2acac6dcd
Matt Domm <mattdomm@gmail.com>
2020-01-22 10:18:34 +0000

    Added Bootstrap form validation, alt text

--- main.js
+++ main.js
@@ -6,3 +6,30 @@ const myForm = document.getElementById('contactForm');
 function resetForm() {
   myForm.reset();
 }
+
+window.onload = resetForm;
+
+(function() {
+  window.addEventListener(
+    'load',
+    function() {
+      // Fetch the form
+      const forms = document.getElementsByClassName('needs-validation');
+      // Loop over to prevent submission
+      const validation = Array.prototype.filter.call(forms, function(form) {
+        form.addEventListener(
+          'submit',
+          function(event) {
+            if (form.checkValidity() === false) {
+              event.preventDefault();
+              event.stopPropagation();
+            }
+            form.classList.add('was-validated');
+          },
+          false
+        );
+      });
+    },
+    false
+  );
+})();
